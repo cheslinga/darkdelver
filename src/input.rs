dd60abf8f9c5ea2200f22dd6ab71135e293252a3
Cole Heslinga <cole.heslinga@gmail.com>
2021-01-15 16:13:34 -0500

    Added diagonal movement

--- src/input.rs
+++ src/input.rs
@@ -1,10 +1,8 @@
 use crate::prelude::*;
 
 pub enum Actions {
-    MoveUp,
-    MoveDown,
-    MoveLeft,
-    MoveRight
+    MoveUp, MoveDown, MoveLeft, MoveRight,
+    MoveUpLeft, MoveUpRight, MoveDownLeft, MoveDownRight
 }
 
 //Grabs the player's keypresses
@@ -19,6 +17,15 @@ pub fn player_input(gs: &mut State, con: &BTerm) {
                 => process_action(gs, Actions::MoveUp),
             VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::K
                 => process_action(gs, Actions::MoveDown),
+
+            VirtualKeyCode::Numpad7 | VirtualKeyCode::Y
+                => process_action(gs, Actions::MoveUpLeft),
+            VirtualKeyCode::Numpad9 | VirtualKeyCode::U
+                => process_action(gs, Actions::MoveUpRight),
+            VirtualKeyCode::Numpad1 | VirtualKeyCode::B
+                => process_action(gs, Actions::MoveDownLeft),
+            VirtualKeyCode::Numpad3 | VirtualKeyCode::N
+                => process_action(gs, Actions::MoveDownRight),
             _ => {}
         }
     }
@@ -30,8 +37,15 @@ fn process_action(gs: &mut State, action: Actions) {
         Actions::MoveRight => try_move(gs, Point::new(1, 0)),
         Actions::MoveUp => try_move(gs, Point::new(0, -1)),
         Actions::MoveDown => try_move(gs, Point::new(0, 1)),
+
+        Actions::MoveUpLeft => try_move(gs, Point::new(-1, -1)),
+        Actions::MoveUpRight => try_move(gs, Point::new(1, -1)),
+        Actions::MoveDownLeft => try_move(gs, Point::new(-1, 1)),
+        Actions::MoveDownRight => try_move(gs, Point::new(1, 1)),
+
         _ => {}
     }
+    gs.refresh_con = true;
 }
 
 //Attempts to move to a (hopefully) walkable tile.
@@ -46,5 +60,4 @@ fn try_move(gs: &mut State, delta: Point) {
         camera.move_camera(dest);
         player.viewshed.as_mut().unwrap().refresh = true;
     }
-    gs.refresh_con = true;
 }
