d075ab46199751952c322a739965d78bea694fab
Cole Heslinga <cole.heslinga@gmail.com>
2021-02-27 16:15:56 -0500

    Updated save files to use CBOR instead of JSON

--- src/saves.rs
+++ src/saves.rs
@@ -3,18 +3,14 @@ use std::fs::*;
 use std::io::*;
 
 pub fn export_world(world: &World) {
-    let jsonstring = serde_json::to_string(world).expect("Could not serialize world data!");
-
-    let mut file = File::create("saves/current.json").expect("Could not create save file!");
-    file.write_all(jsonstring.as_bytes())
+    let raw_data = serde_cbor::ser::to_vec(world).expect("Could not serialize world data!");
+    let mut file = File::create("saves/current.save").expect("Could not create save file!");
+    file.write_all(&*raw_data)
         .expect("Could not write to file!");
 }
 
 pub fn load_world(gs: &mut State) {
-    let mut file = File::open("saves/current.json").expect("Could not open file!");
-    let mut filestring = String::new();
-    file.read_to_string(&mut filestring).unwrap();
-
-    let deserialized_world: World = serde_json::from_str(&filestring).unwrap();
+    let mut file = File::open("saves/current.save").expect("Could not open file!");
+    let deserialized_world: World = serde_cbor::de::from_reader(&file).unwrap();
     gs.world = deserialized_world;
 }
